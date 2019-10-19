9098ba06bac8666978a6dda4ad870f9cde24c6a1
denis281542 <deniskuimov@mail.ru>
2019-10-19 12:46:31 +0500

    Initial commit

---
+++ Chat+actiontext/app/javascript/channels/room_channel.js
@@ -0,0 +1,19 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    console.log('yiha, we are ready!')
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+  
+  received(data) {
+
+    $('#msg').append('<div class="message">' + data.content + '</div>')
+    console.log(data)
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
--- auth/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Auth::Application.routes.draw do
-  get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
-  resources :users
-end
-
